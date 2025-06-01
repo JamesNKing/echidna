79b23822bfe420d47be20808447ad9f8bef2b74c
Jamie King <jamieking2004@gmail.com>
2025-05-31 21:06:13 -0500

    fixed main

--- Payload_Type/echidna/main.py
+++ Payload_Type/echidna/main.py
@@ -1,13 +1,4 @@
-#!/usr/bin/env python3
+import mythic_container
+from echidna.mythic import *
 
-import asyncio
-from mythic_container import mythic_container
-from echidna.builder import Echidna
-
-mythic_container.mythic_service.add_payload_type(Echidna())
-
-async def main():
-    await mythic_container.start_and_run_forever()
-
-if __name__ == "__main__":
-    asyncio.run(main())
+mythic_container.mythic_service.start_and_run_forever()
