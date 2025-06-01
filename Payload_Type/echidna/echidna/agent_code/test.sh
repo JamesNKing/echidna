150a87deb4c8afaf3f2c6dcb152df5edc9a40e1e
Jamie King <jamieking2004@gmail.com>
2025-06-01 16:32:24 -0500

    making sure up to date

--- Payload_Type/echidna/echidna/agent_code/test.sh
+++ Payload_Type/echidna/echidna/agent_code/test.sh
@@ -8,5 +8,9 @@ export killdate="2025-12-31"
 export connection_retries="3"
 export working_hours="00:00-23:59"
 export encrypted_exchange_check="T"
-
-
+export callback_host=""
+export AESPSK=""
+export callback_port=""
+export get_uri=""
+export headers=""
+export post_uri=""
